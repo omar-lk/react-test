574c850d27d388189567bf3f010fce68e6954d6d
omar-lk <omar-lk@users.noreply.github.com>
2021-10-13 12:03:58 +0100

    add post method

--- src/component/AddImagePopup.tsx
+++ src/component/AddImagePopup.tsx
@@ -2,7 +2,21 @@ import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 const AddImagePopup = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    console.log(data);
+    fetch("https://jsonplaceholder.typicode.com/albums/1/photos", {
+      method: "POST",
+      body: JSON.stringify({
+        title: data.name,
+        url: data.image
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  };
   return (
     <div className="popup">
       <form onSubmit={handleSubmit(onSubmit)} className="form-container">
